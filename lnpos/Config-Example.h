914378f73f21008ca9a6fd63fad2781bb14314f3
Bota <Bota@Lenovo>
2024-09-13 23:51:40 +0200

    aumentata la precisione nella lettura del livello di carica e abilitato il deep sleep anche durante la ricarica che sembra continuare comunque

--- lnpos/Config-Example.h
+++ lnpos/Config-Example.h
@@ -1,25 +1,33 @@
+#include <Arduino.h>
 //////////SET TO TRUE TO WIPE MEMORY//////////////
-
 bool format = false;
 
 ////////////////////////////////////////////////////////
-////////////LNPOS WILL LOOK FOR DETAILS SET/////////////
-////////OVER THE WEBINSTALLER CONFIG, HOWEVER///////////
 ///////////OPTIONALLY SET HARDCODED DETAILS/////////////
 ////////////////////////////////////////////////////////
 
+///////// OPTIONALLY SET HARDCODED SETTINGS ////////////
 bool hardcoded = false; /// Set to true to hardcode
 
-String lnurlPoS = "https://legend.lnbits.com/lnurldevice/api/v1/lnurl/WTmei,BzzoY5wbgpym3eMdb9ueXr,USD";
-String lnurlATM = "https://legend.lnbits.com/lnurldevice/api/v1/lnurl/W5xu4,XGg4BJ3xCh36JdMKm2kgDw,USD";
+/// FOR OFFLINE POS
+String offlinePoS = "https://demo.lnbits.com/lnpos/api/v1/lnurl/WTmei,BzzoY5wbgpym3eMdb9ueXr,USD";
+
+/// FOR OFFLINE ATM
+String offlineATM = "https://demo.lnbits.com/fossa/api/v1/lnurl/W5xu4,XGg4BJ3xCh36JdMKm2kgDw,USD";
+
+/// FOR GENERATING ONCHAIN ADDRESSES
 String masterKey = "xpub6CJFgwcim8tPBJo2A6dS13kZxqbgtWKD3LKj1tyurWADbXbPyWo11exyotTSUY3cvhQy5Mfj8FSURgpXhc4L2UvQyaTMC36S49JnNJMmcWU";
-String lnbitsServer = "https://legend.lnbits.com";
-String invoice = "37d45d3e1f0d4572a905bad544588d7d";
-String lncurrency = "GBP";
-String lnurlATMMS = "https://mempool.space";
-String lnurlATMPin = "878787";
-String decimalplaces = "2";
+String mempool = "https://mempool.space";
+
+/// FOR ONLINE POS
+String lnbitsServer = "https://demo.lnbits.com";
+String invoice = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+String lnCurrency = "GBP";
 String ssid = "AlansBits";
 String password = "ithurtswhenip";
+
+/// ADDITIONAL SETTINGS
+String securityPin = "878787";  // FOR SETTINGS AND ATM
+String fiatDecimalPlaces = "2"; // FOR OUR JAPANESE FREINDS
 
 //////////////////////////////////////////////////
